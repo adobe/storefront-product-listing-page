e3792adfa45d1cba5d84100079811616bee47419
Kathleen Tynan <ktynan@adobe.com>
2023-09-25 13:23:46 -0400

    msrch-3806: storybook

--- storybook-static/sb-addons/config-storybook-8/manager-bundle.js
+++
@@ -1,3 +0,0 @@
-try{
-var l=__STORYBOOKADDONS__,{addons:o,types:m,mockChannel:h}=__STORYBOOKADDONS__;var f=__STORYBOOKTHEMING__,{CacheProvider:u,ClassNames:b,Global:k,ThemeProvider:x,background:y,color:C,convert:N,create:a,createCache:R,createGlobal:B,createReset:D,css:G,darken:K,ensure:P,ignoreSsrWarning:Y,isPropValid:v,jsx:A,keyframes:E,lighten:H,styled:I,themes:M,typography:W,useTheme:j,withTheme:w}=__STORYBOOKTHEMING__;var s=a({base:"dark",brandTitle:"Product Listing Widgets"});o.setConfig({theme:s});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
