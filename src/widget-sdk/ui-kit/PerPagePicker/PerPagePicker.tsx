544ed4974d89eb4d831898bf515a79d3c4960a77
ani hammond <annhammo@anis-mbp.corp.adobe.com>
2023-10-11 10:33:56 -0500

    MSRCH-3787: copyright text

--- src/widget-sdk/ui-kit/PerPagePicker/PerPagePicker.tsx
+++ src/widget-sdk/ui-kit/PerPagePicker/PerPagePicker.tsx
@@ -1,3 +1,12 @@
+/*
+Copyright 2024 Adobe
+All Rights Reserved.
+
+NOTICE: Adobe permits you to use, modify, and distribute this file in
+accordance with the terms of the Adobe license agreement accompanying
+it.
+*/
+
 import { Menu, Transition } from '@headlessui/react';
 import { Fragment, FunctionalComponent, h } from 'preact';
 import { useState } from 'preact/hooks';
