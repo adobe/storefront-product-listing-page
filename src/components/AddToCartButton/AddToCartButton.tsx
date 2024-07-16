0042d2c0d209dde09ba72aa5773715ffab1e977d
Yugandhar Yashwantrao Kamdi <yugandharya@adobe.com>
2024-07-17 00:51:31 +0530

    Fix lint errors

--- src/components/AddToCartButton/AddToCartButton.tsx
+++ src/components/AddToCartButton/AddToCartButton.tsx
@@ -9,8 +9,8 @@ it.
 
 import { FunctionComponent } from 'preact';
 
-import PlusIcon from '../../icons/plus.svg';
 import { useSensor } from '../../context';
+import PlusIcon from '../../icons/plus.svg';
 
 export interface AddToCartButtonProps {
   onClick: (e: any) => any;
@@ -21,9 +21,9 @@ export const AddToCartButton: FunctionComponent<AddToCartButtonProps> = ({
   const { screenSize } = useSensor();
 
   return (
-    <div className='ds-sdk-add-to-cart-button h-[40px] w-[40px] lg:h-[100%] lg:w-full'>
+    <div className="ds-sdk-add-to-cart-button h-[40px] w-[40px] lg:h-[100%] lg:w-full">
       <button
-        className='flex items-center justify-center h-[100%] w-full gap-2 lg:px-big lg:py-[10px]'
+        className="flex items-center justify-center h-[100%] w-full gap-2 lg:px-big lg:py-[10px]"
         onClick={onClick}
       >
         <PlusIcon className="w-[14px]" />
