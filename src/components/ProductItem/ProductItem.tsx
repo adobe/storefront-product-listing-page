aafee87be8fa6072e8e7c0e1ef531a95c0834038
Anton Kulakou <anton.k@paxento.com>
2024-09-24 21:09:41 +0300

    fix images resizing

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -104,7 +104,7 @@ export const ProductItem: FunctionComponent<ProductProps> = ({
   if (optimizeImages) {
     optimizedImageArray = generateOptimizedImages(
       productImageArray,
-      imageBaseWidth ?? 200
+      imageBaseWidth ?? 278
     );
   }
 
@@ -286,7 +286,7 @@ export const ProductItem: FunctionComponent<ProductProps> = ({
                   <div className="ds-sdk-product-item__product-name text-black capitalize leading-[1.2] tracking-[0.5px] pt-1.5 font-normal text-[1rem] font-['PlayfairDisplay-Bold'] text-center hover:text-[#666666]">
                     {product.name !== null && htmlStringDecode(product.name)}
                   </div>
-                  <div className="block">{getProductAttribute('mini_description')}</div>
+                  <div className="block">{getProductAttribute('profumo_per')}</div>
                   <ProductPrice
                       item={refinedProduct ?? item}
                       isBundle={isBundle}
