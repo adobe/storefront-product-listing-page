9fef2d1823b3bd54f8d291056a7b64236d914b96
Yugandhar Yashwantrao Kamdi <yugandharya@adobe.com>
2024-09-26 17:39:13 +0530

    WAF:434: Fix product cards with invalid secondary images

--- src/components/ImageHover/ImageHover.tsx
+++ src/components/ImageHover/ImageHover.tsx
@@ -18,18 +18,21 @@ export interface ImageCarouselProps {
 export const ImageHover: FunctionComponent<ImageCarouselProps> = ({
   images,
 }) => {
+    const backImage = images.length > 1 
+    ? (typeof images[1] === 'object' ? images[1].src : images[1])
+    : '';
 
     return (
       <>
-        {images.length > 1 && (
-          <link rel="preload" as="image" href={typeof images[1] === 'object' ? images[1].src : images[1]} />
+        {backImage && (
+          <link rel="preload" as="image" href={backImage} />
         )}
           <meta itemProp="image" content={typeof images[0] === 'object' ? images[0].src : images[0]} />
         <div class="relative w-full pb-[122.22%]">
-          <div class="ds-sdk-product-image-hover absolute h-full w-full m-auto bg-cover bg-no-repeat bg-position-center"
+          <div class={`ds-sdk-product-image ${backImage ? 'hover-enabled' : ''} absolute h-full w-full m-auto bg-cover bg-no-repeat bg-position-center`}
               style={{
                 '--image-url': `url('${typeof images[0] === 'object' ? images[0].src : images[0]}')`,
-                '--hover-url': `url('${typeof images[1] === 'object' ? images[1].src : images[1]}')`,
+                '--hover-url': `url('${backImage}')`,
               }}
             />
         </div>
