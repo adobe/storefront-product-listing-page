db8c3432a2dc5861fc53519c4fea2fa76b2637e4
Anton Kulakou <anton.k@paxento.com>
2024-11-15 20:04:26 +0300

    added display 4 products depends on dimension of the browser window

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -14,6 +14,7 @@ export const DEFAULT_PAGE_SIZE_OPTIONS = '12,24,36';
 export const DEFAULT_MIN_QUERY_LENGTH = 3;
 export const PRODUCT_COLUMNS = {
   desktop: 5,
+  desktopSmall: 4,
   tablet: 3,
   mobile: 2,
 };
