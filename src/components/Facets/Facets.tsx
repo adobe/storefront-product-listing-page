24a295da178564f77e8237f5e9ae3b05b5e4084e
Raul Hudea <rhudea@adobe.com>
2025-01-09 15:23:51 +0200

    Add size sorting

--- src/components/Facets/Facets.tsx
+++ src/components/Facets/Facets.tsx
@@ -27,6 +27,35 @@ interface FacetsProps {
   displayFilter?: () => void;
 }
 
+const sizeFacetsOrder = [
+  'A', 'AB', 'B', 'C', 'CD', 'D', 'DD', 'E', 'EF', 'F', 'GH', 'IJ',
+  'NO SIZE', 'NB8', 'NS', 'ONE SIZE',
+  'PPET', 'PTSM', 'PML', 'Q', 'Q+', 'QT',
+  'XS', 'X SMALL/SMALL', 'XS SHORT',
+  'S', 'SMALL TALL', 'S/5', 'S SHORT', 'SSHORT', 'S/D-DD', 'S/M', 'ST',
+  'M', 'M/6', 'M SHORT', 'MSHORT', 'M PLUS', 'MEDIUM TALL', 'M/T', 'MT', 'M/D-DD', 'M/DD', 'M/L', 'M-L',
+  'L', 'L/7', 'L SHORT', 'LT', 'TL', 'T', 'L PLUS', 'L-X', 'Large-Extra', 'L/D-DD', 'L/DD', 'L/XL',
+  'XL', 'XL/8', 'XL PLUS', 'XL/D-DD', 'XL/1X', 'XL/2X', 'XL SHORT', 'XLT',
+  '0-3', '0M-6M', '0M-6M', '0M-6M', '0-6M',
+  '1', '1X', '1PLUS', '1-2', '1/2X', '1X-3X', '1.5', '1X/2X', '1XT/2XT',
+  '2', '2-4', '2.5', '2T', '2X', '2XB', '2XL', 'XXL', 'XXLP', 'XXL/9', '2XL SHORT', '2XT', '2XLT', '2PLUS', '2XL PLUS', '2/3', '2X/3X', '2/3T',
+  '3', '3-6', '3P', '3MTH', '3T', '3.5', '3XL', 'XXXL/10', '3XB', '3XT', '3XT/4XT', '3X', '3PLUS', '3/4X', '3X/4X',
+  '4T', '4', '4.5', '4X', '4XB', '4XL', '4XL TAL', '4XT', '4/5', '4-5P', '4/5T', '4X-6X', '4X-7X',
+  '5', '5X', '5XL', '5XT', '5.5', '5/6', '5-6 1/2', '5/6X', '5X/6X', '5-7', '5-9',
+  '6', '6MTH', '6/6X', '6X', '6XL', '6XLT', '6PLUS', '6.5', '6/7', '6-7 1/2', '6-9', '6-9 MONTHS', '6-10', '6-12', '6-12M',
+  '7', '7XL', '7.5', '7-8', '7/8', '7-8 1/2', '7-9', '7 1/2-9',
+  '8', '8XL', '8.5', '8/9', '8-10', '8-12',
+  '9', '9MTH', '9XL', '9X', '9.5', '9-10', '9-11', '9-11_1', '9-12', '09/12/19',
+  '10', '10X', '10XL', '10.5', '10/11', '10-12', '10/12', '10-13', '10/13/19', '10-14', '10-15',
+  '11', '11.5', '11-12', '11-13',
+  '12', '12MTH', '12.5', '12/13', '12-14', '12/14', '12/14/19', '12-18M', '12-24', '12-24M',
+  '13', '13-1', '13.5', '14', '14W', '14/16', '15', '16', '16W', '16-18', '18', '18W', '18MTH', '18M-24M',
+  '20', '20W', '22', '22W', '24', '24W', '24MTH', '26', '26W', '28', '28W',
+  '30', '30W', '32', '34', '34A', '36', '38', '40', '42', '50',
+  'YS', 'YM', 'YL', 'YXL',
+  'IPHONE 11', 'IPHONE11PM', 'IPhone11PR', 'IPHONE 12', 'IPHONE12MI', 'IPHONE12PM',
+];
+
 export const scrollFilter = (
   event: any,
   displayFunction: (() => void) | undefined,
@@ -134,6 +163,26 @@ export const Facets: FunctionComponent<FacetsProps> = ({
               </div>
               {searchFacetsSliced?.map((facet, index) => {
                 const bucketType = facet?.buckets[0]?.__typename;
+                if (facet?.attribute === 'pim_sku_size_filter') {
+                    facet?.buckets.sort((a, b) => {
+                      const indexA = sizeFacetsOrder.indexOf(a.title);
+                      const indexB = sizeFacetsOrder.indexOf(b.title);
+
+                      if (indexA !== -1 && indexB !== -1) {
+                        return indexA - indexB;
+                      }
+
+                      if (indexA !== -1) {
+                        return -1;
+                      }
+
+                      if (indexB !== -1) {
+                        return 1;
+                      }
+
+                      return a.title.localeCompare(b.title);
+                    });
+                }
                 switch (bucketType) {
                   case 'ScalarBucket':
                     return (
