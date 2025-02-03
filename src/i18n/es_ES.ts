c98808522b4f97f69634eb3510acd27545dab03a
benjaminkalk <bkalk@adobe.com>
2025-02-03 15:40:39 -0600

    Release/2.1.2 2 (#17)
    * add workflows
    
    * add workflows
    
    * add workflows
    
    * Update Add to Cart Translation for english french and US and fix TS error
    
    * remove package-lock.json in favor of yarn.lock for build
    
    * workflows
    
    * new workflows
    
    * npm-spectrum-release, npm-react-release, npmjs-local, npm-adobe-release-local, npm-adobe-release
    
    * remove yarn.lock
    
    * yarn.lock
    
    * verbose
    
    * verbose
    
    * verbose
    
    * verbose
    
    * engine-strict=true
    
    * engine-strict=false
    
    * engine-strict=false
    
    * engine-strict=false
    
    * engine-strict=false
    
    * engine-strict=false
    
    * merge develop changes
    
    * yarn.lock
    
    * remove verbose, lint
    
    * remove format
    
    * add format
    
    * lint
    
    * update workflows
    
    * set eslint to ignore test files
    
    * set eslint to ignore test files
    
    * set eslint to ignore test files
    
    * formatting fix
    
    * formatting fix
    
    * formatting fix
    
    * update asset name to match artifact output
    
    * remove comments in npmrc file
    
    * add artifact entrypoint to env vars
    
    * add release pull requests
    
    * update to properly gate prod releases for particular release branches
    
    * try qa build in prod env (#8)
    
    * try qa build in prod env
    
    * lock terser version
    
    * pin tailwind
    
    * pin babel, webpack, babel-ts-plugin
    
    * just pin webpack
    
    ---------
    
    Co-authored-by: Kayden Althen <sharkySharks@users.noreply.github.com>
    
    * update if statement to match a pattern
    
    * MSRCH-4892: Update es_ES.ts (#15)
    
    * Update es_ES.ts
    
    * Updates from editor
    
    * MSRCH-4878: Bundle product in store view french do not show price in category view (#16)
    
    Co-authored-by: benjaminkalk <bkalk@adobe.com>
    
    * update version
    
    ---------
    
    Co-authored-by: meadows <meadows@adobe.com>
    Co-authored-by: David Meadows <dmeadows@users.noreply.github.com>
    Co-authored-by: Kayden Althen <althen@adobe.com>
    Co-authored-by: Kayden Althen <sharkySharks@users.noreply.github.com>
    Co-authored-by: Oleg Posyniak <posyniak@adobe.com>
    Co-authored-by: Anna Bukatar <abukatar@adobe.com>

--- src/i18n/es_ES.ts
+++ src/i18n/es_ES.ts
@@ -42,7 +42,7 @@ export const es_ES = {
     products: '{totalCount} productos',
   },
   ProductCard: {
-    addToCart: 'Agregar al Carrito',
+    addToCart: 'Añadir a la cesta',
     asLowAs: 'Por solo {discountPrice}',
     startingAt: 'A partir de {productPrice}',
     bundlePrice: 'Desde {fromBundlePrice} hasta {toBundlePrice}',
